136669b928653f6baaea7a87fef08bfebcf8e908
Andrespipe1 <andresftma@gmail.com>
2024-12-05 14:19:56 -0500

    .

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import { Mongoose } from 'mongoose';
 import app from './server.js'
+import connection from './database.js';
+
+connection()
 
 app.listen(app.get('port'),()=>{
     console.log(`Server ok on http://localhost:${app.get('port')}`);
